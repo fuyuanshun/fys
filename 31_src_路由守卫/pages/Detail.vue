a035ff3500c7d61f9304b2d96f9971c32cf80a8f
fuyuanshun <fuyuanshun521@gmail.com>
2024-03-22 22:55:59 +0800

    路由守卫

---
+++ 31_src_路由守卫/pages/Detail.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul>
+        <li>消息编号：{{ id }}</li>
+        <li>{{ content }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name : 'Detail',
+    props:['id', 'content']
+}
+</script>
